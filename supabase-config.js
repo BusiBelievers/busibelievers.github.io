dece98161312f4c3653f3d5730f3eb43700599af
Bernard Mitchell <jemlives@gmail.com>
2026-03-14 20:52:12 -0500

    Set Supabase publishable anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
-window.BUSI_SUPABASE = {
-url: "",
-anonKey: "",
+window.BUSI_SUPABASE = { 
+url: "https://pocdjxubtdxyqyntlxnt.supabase.co",
+anonKey: "sb_publishable_6k8ZnNuxryYwqOPaYbGsnA__cxt_gzM",
 
 isConfigured: function(){
 return Boolean(this.url && this.anonKey && window.supabase);
